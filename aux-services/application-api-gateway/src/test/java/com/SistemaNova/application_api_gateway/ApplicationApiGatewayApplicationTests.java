566d526a1be4ea60f0562ae883562986f45130f0
RaphaelRegis <raph4life17@gmail.com>
2025-07-21 23:30:44 -0300

    feat: implementacao do api gateway para os microsservicos

---
+++ aux-services/application-api-gateway/src/test/java/com/SistemaNova/application_api_gateway/ApplicationApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SistemaNova.application_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
